d8654e174c233574dd91c0d24186db375665ebe0
Juako <complots.es@gmail.com>
2026-04-28 22:42:00 +0200

    arreglo de laravel 13

--- routes/console.php
+++ routes/console.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Console\Commands\CreateDueEditorialRunsCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Artisan::starting(function () {
-    Artisan::resolveCommands([
-        CreateDueEditorialRunsCommand::class,
-    ]);
-});
